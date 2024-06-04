c18e0aa8d1d142d1eac6d3b0f4b51968f377bd04
ChernokozhevDmitry <che.dubna@gmail.com>
2024-06-04 23:03:00 +0300

    gtest while with error

--- test_version.cpp
+++ test_version.cpp
@@ -1,14 +1,8 @@
-#define BOOST_TEST_MODULE test_version
-
+#include "gtest/gtest.h" 
 #include "lib.h"
 
-#include <boost/test/unit_test.hpp>
-
-BOOST_AUTO_TEST_SUITE(test_version)
-
-BOOST_AUTO_TEST_CASE(test_valid_version)
+TEST(Version, GreaterNull)
 {
-    BOOST_CHECK(version() != 100);
-}
-
-BOOST_AUTO_TEST_SUITE_END()
+    EXPECT_EQ(0, 1);
+        //   ASSERT_TRUE(version() > 0);
+} 
